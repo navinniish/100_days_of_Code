7b6b222ddabd67f8b494526313728abdaef0430e
Navin Nishanth <68501003+navinniish@users.noreply.github.com>
2023-08-20 20:56:50 +0530

    Update and rename SQL Day 1 to SQL Day 1.sql

--- SQL Day 1.sql
+++ SQL Day 1.sql
@@ -1,8 +1,8 @@
-#Create Statements
+/*Create_Statements*/
 
 Create Table Customers_21 (Cust_Id int(50) primary key, Cust_First_Name varchar(100), Cust_Last_name varchar(100), Cust_Phone_No varchar(12), Cust_Address varchar(1000));
 
-#Insert Statements
+/*Insert_tatements*/
 
 INSERT INTO Customers_21 (Cust_Id, Cust_First_Name, Cust_Last_Name, Cust_Phone_No, Cust_Address)
 VALUES (0001, 'Navin', 'Nishanth', '9999999999', 'No 2, Virugambakkam');
@@ -11,11 +11,11 @@ VALUES (0001, '', 'Nishanth', '', 'No 3, Virugambakkam');
 INSERT INTO Customers_21 (Cust_Id, Cust_First_Name, Cust_Last_Name, Cust_Phone_No, Cust_Address)
 VALUES (null, null, 'Nishanth', '', 'No 4, Virugambakkam');
 
-#Select Statements
+/*Select_Statements*/
 
 SELECT * FROM [Customers_21]
 
-#It displays number of rows we inserted
+/*It displays number of rows we inserted*/
 
 SELECT * FROM [Customers_21] WHERE Cust_First_Name IS NULL (Returns null rows)
 
@@ -29,7 +29,7 @@ SELECT * FROM [Customers_21] where   Cust_Last_name like '%sh%' (Used a wildcard
 
 SELECT * FROM [Customers_21] where   Cust_Last_name like '__sh%' (Used a wildcard and a 2 underscores to denote that I dunno 2 words)
 
-Update Statements :
+/*Update Statements :*/
 
 UPDATE [Customers_21] 
 SET Cust_Id = 3 
@@ -39,7 +39,7 @@ UPDATE [Customers_21]
 SET Cust_First_Name = "Niish"
 WHERE Cust_First_Name IS NULL; (Updates the value in the customer name)
 
-Alter Statements:
+/*Alter Statements:*/
 
 ALTER TABLE Customers_21
 RENAME TO New_Customers_21; (Rename the table)
@@ -58,10 +58,10 @@ delete * from New_Customers_21 where Cust_id = 3 (deletes the entire row)
 
 Truncate table New_Customers_21
 
-Joins Basic :
+/*Joins Basic :
 
 Inner Join : Its gives out only the common value
-Left and Right Join : Based upon the tables you join on left or right the value changes
+Left and Right Join : Based upon the tables you join on left or right the value changes*/
 
 select * from customers join orders on customers.customerId = orders.customerId (Number of records 196)
 
